69859a8f1950914d5a639c141a1ec5871df80fcb
Nguyen Duong <muoi07052001@gmail.com>
2023-03-08 22:57:23 +0700

    Change 3d Earth model

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -5,11 +5,11 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
 const Earth = () => {
-  const earth = useGLTF('./planet/scene.gltf');
+  const earth = useGLTF('./windy_planet/scene.gltf');
   return (
     <primitive
       object={earth.scene}
-      scale={2.5}
+      scale={2}
       position-y={0}
       rotation-y={0}
     ></primitive>
